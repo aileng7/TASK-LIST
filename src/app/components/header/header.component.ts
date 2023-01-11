f41662bd19301c500069b414759688671c588b0e
aileng7 <gonzalez_ailen@hotmail.com>
2023-01-11 16:56:35 -0300

    button-component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  title:string = 'My Task List'
+
+
+export class HeaderComponent implements OnInit {
+  title:string = 'My Task List';
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+  toggleAddTask(){
+    console.log("toggleAddTask!");
+  }
 }
